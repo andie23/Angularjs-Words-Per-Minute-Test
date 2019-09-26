da80f642620055c1a6bb4d067500a8ac880985b9
Andrew Mfune <andiemfune@gmail.com>
2019-09-26 11:14:29 -0800

    Before changing routes, we're checking if participant is logged in.
    If not logged in, redirecting to login page

--- app/controllers.js
+++ app/controllers.js
@@ -1,3 +1,11 @@
+TypingTestModule.run(function($rootScope, $location, authService){
+    $rootScope.$on('$routeChangeStart', function(event) {
+        if (!authService.isLoggedIn()){
+            appAlert.error("I won't trust you until I know your Reference Code..");
+            $location.url('/');
+        }
+    })
+})
 TypingTestModule.controller('MenuController', function($scope, authService){
     $scope.participantName = authService.getParticipantName();
 })
