3a8fba48e9750857c4d446a37f5df8bdba694f22
Andrew Mfune <andiemfune@gmail.com>
2019-09-28 11:44:07 -0800

    Code refactor

--- app/alert.js
+++ app/alert.js
@@ -1,23 +1,17 @@
 var appAlert = {};
 
-appAlert.success= function(message){
-    $('#alert-section').html(
-        '<div class="alert alert-success alert-dismissible fade show text-center" role="alert">'+
+appAlert.setAlert = function(message, styleClass){
+    $('<div class="alert ' + styleClass + ' alert-dismissible fade show text-center" role="alert">'+
             message +
         '<button type="button" class="close" data-dismiss="alert" aria-label="Close">'+
         '<span aria-hidden="true">&times;</span>' +
         '</button>'  +
-        '</div>'
-    );
+        '</div>').appendTo('#alert-section');
+}
+appAlert.success= function(message){
+    appAlert.setAlert(message, 'alert-success');
 }
 
 appAlert.error = function(message){
-    $('#alert-section').html(
-        '<div class="alert alert-danger alert-dismissible fade show text-center" role="alert">'+
-            message +
-        '<button type="button" class="close" data-dismiss="alert" aria-label="Close">'+
-        '<span aria-hidden="true">&times;</span>' +
-        '</button>'  +
-        '</div>'
-    );
+    appAlert.setAlert(message, 'alert-danger');
 }
